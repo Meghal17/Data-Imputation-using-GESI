c295b8a8e715cd432c3c97083f8e1128197102ef
Meghal Darji <29067548+Meghal17@users.noreply.github.com>
2022-05-01 18:37:57 -0400

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,7 +1,11 @@
-BASE = 'E:/UWindsor/Term 4/Data Mining/Project/Datasets/'
+#BASE: directory cotaining the datasets and metadata
+BASE = 'main/directory/path'
+#BASE_PATH: directory containing incomplete datasets
 BASE_PATH = BASE + 'Incomplete_Datasets_Without_Labels/'
 META = 'List_of_Datasets.xlsx'
 ORIGINAL_BASE_PATH = BASE + 'Original_Datasets_Without_Labels/'
-IMPUTED_NUMERICAL = 'E:/UWindsor/Term 4/Data Mining/Project/Github/Imputed/Numerical'
-IMPUTED_CAT = 'E:/UWindsor/Term 4/Data Mining/Project/Github/Imputed/Categorical'
-IMPUTED_COMBINED = 'E:/UWindsor/Term 4/Data Mining/Project/Github/Imputed/Numerical'
+
+#Paths to store imputed dataset files
+IMPUTED_NUMERICAL = 'path/to/store/imputed/numerical/datasets'
+IMPUTED_CAT = 'path/to/store/imputed/categorical/datasets'
+IMPUTED_COMBINED = 'path/to/store/imputed/combined/datasets'
